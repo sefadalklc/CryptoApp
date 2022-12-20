ae7a53bbaf104417ada6d80400d69c07863b1fbe
Soner Türkoğlu <soner_12384@hotmail.com>
2022-12-21 00:02:44 +0300

    yeni eklemeler

---
+++ app/src/main/java/com/example/cryptoapp/CurrencyRVAdapter.java
@@ -0,0 +1,23 @@
+package com.example.cryptoapp;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recylerview.widget.RecyclerView;
+public class CurrencyRVAdapter extends RecylerView.Adapter<CurrencyRVAdapter.ViewHolder>{
+    @NonNull
+    @Override
+
+    public CurrencyRVAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CurrencyRVAdapter.ViewHolder holder, int position){
+
+    }
+
+    @Override
+    public int getItemCount(){
+        return 0;
+    }
+
+}
