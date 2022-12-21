b5fb4cc45191fd3af96e682a08e504be7381aea5
omerfurkank <omerfurkankilic@yandex.com>
2022-12-21 07:15:33 +0300

    3.commit

--- app/src/main/java/com/example/cryptoapp/MainActivity.java
+++ app/src/main/java/com/example/cryptoapp/MainActivity.java
@@ -6,11 +6,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText searchEdt;
     private RecyclerView currenciesRV;
     private ProgressBar loadingPB;
+    private ArrayList<CurrencyRVModal> currencyRVModalArrayList;
+    private CurrencyRVAdapter currencyRVAdapter;
 
 
     @Override
@@ -20,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
         searchEdt = findViewById(R.id.idEdtSearch);
         currenciesRV = findViewById(R.id.idRVCurrencies);
         loadingPB = findViewById(R.id.idPBLoading);
+        currencyRVModalArrayList = new ArrayList<>();
+        currencyRVAdapter = new CurrencyRVAdapter(currencyRVModalArrayList,this);
+        currenciesRV.setLayoutManager(new LinearLayoutManager(this));
+        currenciesRV.setAdapter(currencyRVAdapter);
+        getCurrencyData();
+    }
+
+    private void getCurrencyData(){
+        String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
     }
 }
